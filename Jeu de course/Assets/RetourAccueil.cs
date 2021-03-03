9d464f0f55d101a65aed431957ad13d65373b643
Vincent Anctil <vincentanctil16@gmail.com>
2021-03-03 14:31:33 -0500

    Accueil_et_Menu

---
+++ Jeu de course/Assets/RetourAccueil.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetourAccueil : MonoBehaviour
+{
+    public void Retour()
+	{
+		SceneManager.LoadScene(0);
+	}
+
+	public void OpenInformations()
+	{
+		SceneManager.LoadScene(2);
+	}
+}
