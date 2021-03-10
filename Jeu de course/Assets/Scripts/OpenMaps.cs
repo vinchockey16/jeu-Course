5dec01a76454f0af6674bcd33fc2ebca37146bc2
Vincent Anctil <vincentanctil16@gmail.com>
2021-03-10 15:13:25 -0500

    Map 1

---
+++ Jeu de course/Assets/Scripts/OpenMaps.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenMaps : MonoBehaviour
+{
+    public void OpenMap1()
+	{
+		SceneManager.LoadScene(3);
+	}
+}
