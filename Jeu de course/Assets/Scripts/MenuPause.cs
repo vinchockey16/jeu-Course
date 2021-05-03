0445996820c642e1df99ed3bfd2161fc1797f79e
Vincent Anctil <vincentanctil16@gmail.com>
2021-05-03 12:09:30 -0400

    Course 1 et 2

---
+++ Jeu de course/Assets/Scripts/MenuPause.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+    public static bool JeuEnPause = false;
+    public GameObject menuPauseUI;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            if(JeuEnPause){
+                Resume();
+            }
+            else{
+                Pause();
+            }
+        }
+        
+    }
+
+    public void Resume(){
+        menuPauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        JeuEnPause = false;
+    }
+
+    void Pause(){
+        menuPauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        JeuEnPause = true;
+    }
+
+    public void ChargerMenu(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(1); // Scene du menu
+        menuPauseUI.SetActive(false);
+    }
+
+    public void QuitterJeu(){
+        Debug.Log("Jeu quitté");
+        Application.Quit();
+    }
+}
---
+++ Jeu de course/Assets/Scripts/countDown.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class countDown : MonoBehaviour
+{
+    public int countdownTemps;
+    public TextMeshProUGUI countdownAffichage;
+
+    public void Start()
+    {
+        StartCoroutine(CountDownDepart());
+    }
+    IEnumerator CountDownDepart(){
+        Time.timeScale = 0;
+        while(countdownTemps > 0){
+            countdownAffichage.text = countdownTemps.ToString();
+            yield return new WaitForSecondsRealtime(1);
+
+            countdownTemps--;
+        }
+
+        countdownAffichage.text = "GO!";
+        Time.timeScale = 1;
+        yield return new WaitForSeconds(1f);
+
+        countdownAffichage.gameObject.SetActive(false);
+        
+    }
+}
