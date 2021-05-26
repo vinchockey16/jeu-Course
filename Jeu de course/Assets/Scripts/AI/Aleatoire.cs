9d744a9984a58636bed7ff1d7cd272c47c276e83
Julien Bernard <julienbernard6906@gmail.com>
2021-05-26 12:23:04 -0400

    BugRésou

--- Jeu de course/Assets/Scripts/AI/Aleatoire.cs
+++ Jeu de course/Assets/Scripts/AI/Aleatoire.cs
@@ -18,7 +18,6 @@ public class Aleatoire : MonoBehaviour
     private int entier = 0;
     private int finY;
     private List<PathNodes> chemin;
-    private int tourner = 0;
     private  int j = 1;
     private float decalageX;
     private float decalageY;
@@ -100,128 +99,124 @@ public class Aleatoire : MonoBehaviour
 
     private void Update()
     {
-            if (tourner == 0)
+        Vector3 vecteur1 = new Vector3((cible.x - transform.position.x), (cible.y - transform.position.y));
+        Vector3 vecteur2 = transform.up;
+
+        angle = Math.Round((Vector3.AngleBetween(vecteur1, vecteur2)) * (180 / Math.PI), 0);
+
+        direction = Convert.ToSingle(angle);
+
+        if (transform.eulerAngles.z < 270 && transform.eulerAngles.z > 90)
+        {
+            if (transform.eulerAngles.z < 270 && transform.eulerAngles.z > 180)
             {
-                    Vector3 vecteur1 = new Vector3((cible.x - transform.position.x), (cible.y - transform.position.y));
-                    Vector3 vecteur2 = transform.up;
+                if (vecteur1.y / vecteur1.x < vecteur2.y / vecteur2.x)
+                {
+                    direction = direction * -1;
+                }
+                else if (vecteur1.y < 0 && vecteur1.x < 0)
+                {
+                    direction = direction * -1;
+                }
 
-                    angle = Math.Round((Vector3.AngleBetween(vecteur1, vecteur2)) * (180 / Math.PI), 0);
-
-                    direction = Convert.ToSingle(angle);
-
-                    if (transform.eulerAngles.z < 270 && transform.eulerAngles.z > 90)
-                    {
-                        if (transform.eulerAngles.z < 270 && transform.eulerAngles.z > 180)
-                        {
-                            if (vecteur1.y / vecteur1.x < vecteur2.y / vecteur2.x)
-                            {
-                                direction = direction * -1;
-                            }
-                            else if ( vecteur1.y < 0 && vecteur1.x < 0)
-                        {
-                            direction = direction * -1;
-                        }
-
-                    }
-                        else if (transform.eulerAngles.z > 90 && transform.eulerAngles.z < 180)
-                        {
-                            if (vecteur1.y / vecteur1.x < vecteur2.y / vecteur2.x &&  vecteur1.x < 0)
-                            {
-                                direction = direction * -1;
-                            }
-                            else if (vecteur1.y / vecteur1.x < vecteur2.y / vecteur2.x && vecteur1.y < 0 && vecteur1.x > 0)
-                            {
-                              direction = direction * 1;
-                            }
-
-                        else if (vecteur1.y / vecteur1.x < vecteur2.y / vecteur2.x && vecteur1.y < 0 && vecteur1.x < 0)
-                        {
-                            direction = direction * -1;
-                        }
-
-                    }
-                        else if (transform.eulerAngles.z <= 180 && transform.eulerAngles.z >= 180)
-                        {
-                            if (vecteur1.y / vecteur1.x > 0)
-                            {
-                                direction = direction * -1;
-                            }
-                        }
-                    }
-                    else if (transform.eulerAngles.z > 270 || transform.eulerAngles.z < 90)
-                    {
-
-                        if (transform.eulerAngles.z > 0 && transform.eulerAngles.z < 90)
-                        {
-                            if (vecteur1.y / vecteur1.x < vecteur2.y / vecteur2.x)
-                            {
-                                direction = direction * -1;
-                            }
-                            else if (vecteur1.y / vecteur1.x > 0 && vecteur1.y > 0)
-                            {
-
-                                direction = direction * -1;
-                            }
-                        }
-                        else if (transform.eulerAngles.z < 360 && transform.eulerAngles.z > 270)
-                        {
-                            if (vecteur1.y / vecteur1.x < vecteur2.y / vecteur2.x && vecteur1.y / vecteur1.x > 0)
-                            {
-                                direction = direction * -1;
-                            }
-                            else if (vecteur1.y / vecteur1.x < 0 && vecteur1.y > 0)
-                            {
-                                direction = direction * 1;
-                            }
-                            else if (vecteur1.y / vecteur1.x < 0 && vecteur1.y < 0)
-                            {
-                                direction = direction * -1;
-                            }
-
-                        }
-                        else if (transform.eulerAngles.z == 0 || transform.eulerAngles.z == 360)
-                        {
-                            if (vecteur1.y / vecteur1.x > 0)
-                            {
-                                direction = direction * -1;
-                            }
-
-                        }
-                    }
-                    else if (transform.eulerAngles.z == 90)
-                    {
-                        if (vecteur1.y / vecteur1.x < 0)
-                        {
-                            direction = direction * -1;
-                        }
-                    }
-                    else if (transform.eulerAngles.z == 270)
-                    {
-                        if (vecteur1.y / vecteur1.x < 0)
-                        {
-                            direction = direction * -1;
-                        }
-                    }
-
-                    if (angle != 0f)
-                    {
-                         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, 0f, (direction / Math.Abs(direction)) * 150f * Time.deltaTime));
-                    }
-                    else { tourner = 1;}
             }
-
-            if (transform.position.x >= cible.x - 30 && transform.position.y >= cible.y - 30 && transform.position.x <= cible.x + 30 && transform.position.y <= cible.y + 30)
+            else if (transform.eulerAngles.z > 90 && transform.eulerAngles.z < 180)
             {
-             if(vitesse < 400)
+                if (vecteur1.y / vecteur1.x < vecteur2.y / vecteur2.x && vecteur1.x < 0)
+                {
+                    direction = direction * -1;
+                }
+                else if (vecteur1.y / vecteur1.x < vecteur2.y / vecteur2.x && vecteur1.y < 0 && vecteur1.x > 0)
+                {
+                    direction = direction * 1;
+                }
+
+                else if (vecteur1.y / vecteur1.x < vecteur2.y / vecteur2.x && vecteur1.y < 0 && vecteur1.x < 0)
+                {
+                    direction = direction * -1;
+                }
+
+            }
+            else if (transform.eulerAngles.z <= 180 && transform.eulerAngles.z >= 180)
+            {
+                if (vecteur1.y / vecteur1.x > 0)
+                {
+                    direction = direction * -1;
+                }
+            }
+        }
+        else if (transform.eulerAngles.z > 270 || transform.eulerAngles.z < 90)
+        {
+
+            if (transform.eulerAngles.z > 0 && transform.eulerAngles.z < 90)
+            {
+                if (vecteur1.y / vecteur1.x < vecteur2.y / vecteur2.x)
+                {
+                    direction = direction * -1;
+                }
+                else if (vecteur1.y / vecteur1.x > 0 && vecteur1.y > 0)
+                {
+
+                    direction = direction * -1;
+                }
+
+            }
+            else if (transform.eulerAngles.z < 360 && transform.eulerAngles.z > 270)
+            {
+                if (vecteur1.y / vecteur1.x < vecteur2.y / vecteur2.x && vecteur1.y / vecteur1.x > 0)
+                {
+                    direction = direction * -1;
+                }
+                else if (vecteur1.y / vecteur1.x < 0 && vecteur1.y > 0)
+                {
+                    direction = direction * 1;
+                }
+                else if (vecteur1.y / vecteur1.x < 0 && vecteur1.y < 0)
+                {
+                    direction = direction * -1;
+                }
+
+            }
+            else if (transform.eulerAngles.z == 0 || transform.eulerAngles.z == 360)
+            {
+                if (vecteur1.y / vecteur1.x > 0)
+                {
+                    direction = direction * -1;
+                }
+
+            }
+        }
+        else if (transform.eulerAngles.z == 90)
+        {
+            if (vecteur1.y / vecteur1.x < 0)
+            {
+                direction = direction * -1;
+            }
+        }
+        else if (transform.eulerAngles.z == 270)
+        {
+            if (vecteur1.y / vecteur1.x < 0)
+            {
+                direction = direction * -1;
+            }
+        }
+
+        if (angle != 0f)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, 0f, (direction / Math.Abs(direction)) * 150f * Time.deltaTime));
+        }
+
+        if (transform.position.x >= cible.x - 30 && transform.position.y >= cible.y - 30 && transform.position.x <= cible.x + 30 && transform.position.y <= cible.y + 30)
+        {
+            if (vitesse < 400)
             {
                 vitesse += 120;
             }
 
-                cible = new Vector3((chemin[j + 1].x - chemin[j].x) * 30f + cible.x, (chemin[j + 1].y - chemin[j].y) * 30f + cible.y);
+            cible = new Vector3((chemin[j + 1].x - chemin[j].x) * 30f + cible.x, (chemin[j + 1].y - chemin[j].y) * 30f + cible.y);
 
-                j++;
-                tourner = 0;
-            }
+            j++;
+        }
     }
     private void FixedUpdate()
     {
